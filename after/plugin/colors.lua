7c320cababfb51bdab118fae9026ac1f59ca9d0b
gordonzu <gzulux@gmail.com>
2023-05-18 20:32:17 -0400

    change default color to gruvbox-material

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,9 @@
 function MyColor(color)
 	--color = color or "tokyonight-night"
 	--color = color or "material"
-	--color = color or "gruvbox-material"
+	color = color or "gruvbox-material"
 	--color = color or "cake16"
-	color = color or "gruvbox"
+	--color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 end
 
